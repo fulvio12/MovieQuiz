19eddb065b578dc56d135bc1bbf4c465a0ba1547
Fulvio Resendes <fgresendes@gmail.com>
2018-03-28 09:54:15 -0300

    Initial Commit

---
+++ MovieQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by fgrmac on 28/03/18.
+//  Copyright © 2018 Fulvio Resendes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
